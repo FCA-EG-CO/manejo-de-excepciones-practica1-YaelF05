2e47dc99a5128fb8b95390bc3e74240fb9e27ecc
FRANCO TOLEDO YAEL <zS23017346@estudiantes.uv.mx>
2025-02-17 12:55:51 -0600

    cambios

--- prueba.java
+++ prueba.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
 public class prueba {
+    public static void verificacion( int numero) throws Exception {
+        if(numero % 2 != 0) throw new Exception("El numero es impar");
+        System.out.println("El numero es par");
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         try{
             System.out.print("Introduzca el numero: ");
             int numero = sc.nextInt();
-            if (numero % 2 != 0) {throw new Exception("El numero es impar");}
-            else {System.out.println("El numero es par");}
+            verificacion(numero);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
